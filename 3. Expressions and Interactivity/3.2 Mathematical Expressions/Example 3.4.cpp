cddbc9d17f14cb93e2123437412d6ccb3d67a6b8
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-01 18:58:15 -0700

    Add files via upload

---
+++ 3. Expressions and Interactivity/3.2 Mathematical Expressions/Example 3.4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double numerator, denominator;
+    cout << "This program shows the decimal value of a fraction.\n";
+    cout << "Enter the numerator: ";
+    cin >> numerator;
+    cout << "Enter the denominator: ";
+    cin >> denominator;
+    cout << "The decimal value is " << numerator/denominator << ".\n";
+}
+
+/*
+ 
+ This program shows the decimal value of a fraction.
+ Enter the numerator: 3
+ Enter the denominator: 16
+ The decimal value is 0.1875.
+ 
+*/
