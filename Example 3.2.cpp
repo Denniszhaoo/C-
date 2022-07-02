634ab19ed77b33446c87e1fc0331cc2d3b3eda40
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-01 18:44:40 -0700

    Add files via upload

---
+++ Example 3.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int length, width, area;
+    cout << "This program calculates the area of a rectangle.\nEnter the length and width of the rectangle separated by a space: ";
+    cin >> length >> width;
+    area=length*width;
+    cout << "The area of the rectangle is " << area << ".\n";
+}
+
+/*
+ 
+ This program calculates the area of a rectangle.
+ Enter the length and width of the rectangle separated by a space: 10 20
+ The area of the rectangle is 200.
+ 
+*/
