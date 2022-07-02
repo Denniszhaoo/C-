9eb54d28feea043ce78b565930148255948bba15
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-02 11:00:01 -0700

    Add files via upload

---
+++ 3. Expressions and Interactivity/3.5 Type Casting/Example 3.10.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number=65;
+    cout << number << endl;
+    cout << static_cast<char>(number) << endl;
+}
+
+/*
+ 
+ 65
+ A
+
+*/
