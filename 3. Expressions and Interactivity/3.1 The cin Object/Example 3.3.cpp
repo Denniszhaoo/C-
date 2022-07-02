9a77cf2d50f75040ee42d1f3d30c422c81dafe4d
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-01 19:00:51 -0700

    Add files via upload

---
+++ 3. Expressions and Interactivity/3.1 The cin Object/Example 3.3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int whole;
+    double fractional;
+    char letter;
+    cout << "Enter an integer, a double, and a character: ";
+    cin >> whole >> fractional >> letter;
+    cout << "Whole: " << whole << "\nFractional: " << fractional << "\nLetter: " << letter << endl;
+}
+
+/*
+ 
+ Enter an integer, a double, and a character: 4 5.7 b
+ Whole: 4
+ Fractional: 5.7
+ Letter: b
+ 
+*/
