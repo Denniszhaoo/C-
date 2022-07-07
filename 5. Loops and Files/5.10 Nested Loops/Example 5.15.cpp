634098da67219d3ebd82517c4f6ca73dd2984a4a
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-06 17:04:09 -0700

    Add files via upload

---
+++ 5. Loops and Files/5.10 Nested Loops/Example 5.15.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ofstream outputFile;
+    outputFile.open("demofile.txt");
+    cout << "Now writing data to the file.\n";
+    outputFile << "Bach\n";
+    outputFile << "Beethoven\n";
+    outputFile << "Mozart\n";
+    outputFile << "Schubert\n";
+    outputFile.close();
+    cout << "Done.\n";
+}
+
+/*
+ 
+ Now writing data to the file.
+ Done.
+
+*/
