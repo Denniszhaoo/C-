313a77353b41e38cf47717856144f860139a8564
Denniszhaoo <108558272+Denniszhaoo@users.noreply.github.com>
2022-07-06 17:48:44 -0700

    Add files via upload

--- 6. Functions/6.5 Passing Data by Value/Example 6.9.cpp
+++ 6. Functions/6.5 Passing Data by Value/Example 6.9.cpp
@@ -1,19 +1,20 @@
+// this program demonstrates that changes to a function parameter have no effect on the original argument; however, the changes to reference variables (as function parameters) have effect on the original argument
 #include <iostream>
 using namespace std;
 
-void changeMe(int);
+void changeMe(int); // if this is changeMe(int &), then the output will be "Now back ... number is 0", since a reference variable as a function parameter allows the changeMe function to access the parameter's original argument
 
 int main() {
     int number=12;
     cout << "Number is " << number << ".\n";
-    changeMe(number);
+    changeMe(number); // the changeMe function passes the value in number as an argument
     cout << "Now back in the main function again, the value of the number is " << number << ".\n";
 }
 
 void changeMe(int myValue) {
     myValue=0;
     cout << "Now the value is " << myValue << ".\n";
-}
+} // even though the parameter variable myValue is changed in the changeMe function, the argument number is not modified, since the myValue variable contains only a copy of the number variable; also, the changeMe function does not have access to the original argument, and when only a copy of an argument is passed to a function, it is said to be passed by value
 
 /*
 
